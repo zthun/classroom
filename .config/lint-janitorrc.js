be11c39e388b04d22853319c125f89788e02a767
Anthony Bonta <zthun.coding@gmail.com>
2023-12-18 09:52:07 -0600

    build: lint

--- .config/lint-janitorrc.js
+++ .config/lint-janitorrc.js
@@ -1,21 +1,34 @@
-const generated = ['node_modules/**'];
-const partialGenerated = ['cspell.json'];
+const generated = ['**/CHANGELOG.md', 'packages/**/dist/**', 'packages/**/docs/**', 'packages/**/stats/**', 'node_modules/**', 'packages/**/node_modules/**', 'package-lock.json', '.yarnrc.yml'];
 
-const markdownFiles = ['*.md', 'courses/**/*.md'];
-const jsonFiles = ['*.json'];
-const prettyFiles = [].concat(markdownFiles).concat(jsonFiles);
-const spellingFiles = [].concat(markdownFiles).concat(jsonFiles);
+const partialGenerated = ['cspell.json', 'lerna.json'];
 
+const esFiles = ['.config/*.js', '*.ts', 'packages/**/src/**/*.ts', 'packages/**/src/**/*.tsx'];
+const htmlFiles = ['packages/**/*.html'];
+const markdownFiles = ['*.md', 'courses/**/*.md', 'packages/**/*.md'];
+const jsonFiles = ['*.json', 'packages/**/*.json'];
+const yamlFiles = ['.circleci/config.yml'];
+const prettyFiles = [].concat(esFiles).concat(htmlFiles).concat(markdownFiles).concat(jsonFiles).concat(yamlFiles);
+const spellingFiles = [].concat(esFiles).concat(htmlFiles).concat(markdownFiles).concat(jsonFiles).concat(yamlFiles);
+
+const esFilesExclude = generated;
+const htmlFilesExclude = generated;
 const markdownFilesExclude = generated;
 const jsonFilesExclude = generated;
+const yamlFilesExclude = generated;
 const prettyFilesExclude = generated.concat(partialGenerated);
 const spellingFilesExclude = generated.concat(partialGenerated);
 
 module.exports = {
+  esFiles,
+  esFilesExclude,
+  htmlFiles,
+  htmlFilesExclude,
   markdownFiles,
   markdownFilesExclude,
   jsonFiles,
   jsonFilesExclude,
+  yamlFiles,
+  yamlFilesExclude,
   prettyFiles,
   prettyFilesExclude,
   spellingFiles,
