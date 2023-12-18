401cd1e9bebdab645230ae2213b12ac5587315ec
Anthony Bonta <zthun.coding@gmail.com>
2023-12-18 10:44:55 -0600

    build: adding api

--- .config/helpful-proxyrc.js
+++ .config/helpful-proxyrc.js
@@ -3,7 +3,8 @@ module.exports = {
     {
       name: 'classroom.local.zthunworks.com',
       paths: {
-        '/': 'classroom-services-web:5173'
+        '/': 'classroom-services-web:5173',
+        '/api': 'classroom-services-api:3000/api'
       }
     }
   ]
