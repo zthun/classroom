c242d410d97069a328fbf2d6615658c430f9931a
Anthony Bonta <zthun.coding@gmail.com>
2024-02-05 07:56:53 -0600

    refactor: signature

--- packages/classroom-api/src/lessons/lessons-list-service.ts
+++ packages/classroom-api/src/lessons/lessons-list-service.ts
@@ -9,8 +9,8 @@ import { IZResourceListService } from '../resource/resource-service';
 export class ZLessonsListService implements IZResourceListService<IZLesson> {
   public constructor(@Inject(ZDatabaseToken) private readonly _dal: IZDatabaseDocument) {}
 
-  public async list(request?: IZDataRequest): Promise<IZPage<IZLesson>> {
-    const count = await this._dal.count(ZClassroomCollection.Lessons, request?.filter);
+  public async list(request: IZDataRequest): Promise<IZPage<IZLesson>> {
+    const count = await this._dal.count(ZClassroomCollection.Lessons, request.filter);
     const data = await this._dal.read<IZLesson>(ZClassroomCollection.Lessons, request);
     return new ZPageBuilder<IZLesson>().data(data).count(count).build();
   }
