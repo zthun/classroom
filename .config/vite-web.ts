f9d99b904ad24cb1eb29d4f944570296ce4ce3aa
Anthony Bonta <zthun.coding@gmail.com>
2024-02-06 20:54:12 -0600

    build: add visualizer for web projects

--- .config/vite-web.ts
+++ .config/vite-web.ts
@@ -1,9 +1,15 @@
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
 export function defineWeb(dir: string) {
   return defineConfig({
-    plugins: [tsConfigPaths()],
+    plugins: [
+      tsConfigPaths(),
+      visualizer({
+        filename: `${dir}/stats/analysis.html`
+      })
+    ],
     server: {
       host: '0.0.0.0',
       strictPort: true
